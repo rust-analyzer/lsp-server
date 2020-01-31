e6a9bdc3e8b9e3a49bf6f023745856ad1f78cc7a
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-01-31 13:26:49 +0100

    Print blocking status of stdout/stdin

--- src/stdio.rs
+++ src/stdio.rs
@@ -1,6 +1,5 @@
 use std::{
-    io::{self, stdin, stdout},
-    os::unix::io::AsRawFd,
+    io::{self, stdin, stdout, ErrorKind},
     thread,
 };
 
@@ -14,22 +13,33 @@ pub(crate) fn stdio_transport() -> (Sender<Message>, Receiver<Message>, IoThread
     let writer = thread::spawn(move || {
         let stdout = stdout();
 
-        let flags = unsafe { libc::fcntl(stdout.as_raw_fd(), libc::F_GETFL, 0) };
-        eprintln!("stdout blocking = {:?}", (flags & libc::O_NONBLOCK) == libc::O_NONBLOCK);
-
         let mut stdout = stdout.lock();
-        writer_receiver.into_iter().try_for_each(|it| it.write(&mut stdout))?;
-        Ok(())
+        let res = writer_receiver.into_iter().try_for_each(|it| it.write(&mut stdout));
+
+        res.map_err(|err| {
+            if let ErrorKind::WouldBlock = err.kind() {
+                log::error!(
+                    "stdout returned WouldBlock, is_non_blocking(stdout) = {:?}",
+                    is_non_blocking(&stdout)
+                )
+            }
+            err
+        })
     });
     let (reader_sender, reader_receiver) = bounded::<Message>(0);
     let reader = thread::spawn(move || {
         let stdin = stdin();
 
-        let flags = unsafe { libc::fcntl(stdin.as_raw_fd(), libc::F_GETFL, 0) };
-        eprintln!("stdin blocking = {:?}", (flags & libc::O_NONBLOCK) == libc::O_NONBLOCK);
-
         let mut stdin = stdin.lock();
-        while let Some(msg) = Message::read(&mut stdin)? {
+        while let Some(msg) = Message::read(&mut stdin).map_err(|err| {
+            if let ErrorKind::WouldBlock = err.kind() {
+                log::error!(
+                    "stdin returned WouldBlock, is_non_blocking(stdin) = {:?}",
+                    is_non_blocking(&stdin)
+                )
+            }
+            err
+        })? {
             let is_exit = match &msg {
                 Message::Notification(n) => n.is_exit(),
                 _ => false,
@@ -63,4 +73,15 @@ impl IoThreads {
             Err(_err) => panic!("writer panicked"),
         }
     }
+}
+
+#[cfg(unix)]
+fn is_non_blocking(fd: &impl std::os::unix::io::AsRawFd) -> bool {
+    let flags = unsafe { libc::fcntl(fd.as_raw_fd(), libc::F_GETFL, 0) };
+    (flags & libc::O_NONBLOCK) == libc::O_NONBLOCK
+}
+
+#[cfg(not(unix))]
+fn is_non_blocking<T>(_fd: &T) -> bool {
+    false
 }
