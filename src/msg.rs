c8238b0d6ccc1600237d93767910932876fc35a9
Jeremy Kolb <kjeremy@gmail.com>
2021-03-31 21:43:42 -0400

    ServerCancelled error for LSP 3.17 and clarifying documentation

--- src/msg.rs
+++ src/msg.rs
@@ -98,7 +98,7 @@ pub struct ResponseError {
 #[derive(Clone, Copy, Debug)]
 #[allow(unused)]
 pub enum ErrorCode {
-    // Defined by JSON RPC
+    // Defined by JSON RPC:
     ParseError = -32700,
     InvalidRequest = -32600,
     MethodNotFound = -32601,
@@ -109,9 +109,27 @@ pub enum ErrorCode {
     ServerNotInitialized = -32002,
     UnknownErrorCode = -32001,
 
-    // Defined by the protocol.
+    // Defined by the protocol:
+    /// The client has canceled a request and a server has detected
+    /// the cancel.
     RequestCanceled = -32800,
+
+    /// The server detected that the content of a document got
+    /// modified outside normal conditions. A server should
+    /// NOT send this error code if it detects a content change
+    /// in it unprocessed messages. The result even computed
+    /// on an older state might still be useful for the client.
+    ///
+    /// If a client decides that a result is not of any use anymore
+    /// the client should cancel the request.
     ContentModified = -32801,
+
+    /// The server cancelled the request. This error code should
+    /// only be used for requests that explicitly support being
+    /// server cancellable.
+    ///
+    /// @since 3.17.0
+    ServerCancelled = -32802,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
